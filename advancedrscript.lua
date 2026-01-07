d625a02cb8f86be28ec23e8513012f1b388f3c70
jyhori <jyhori228@gmail.com>
2026-01-07 18:49:43 +0300

    Create advancedrscript.lua

---
+++ advancedrscript.lua
@@ -0,0 +1,19 @@
+needed nickname = true{}
+verify.key = false{}
+get.robux "2147483647" = true{}
+*/ digital.code.allow = {robux, nickname, data, nokey}
+robux.current.quantity = "2147483647"
+#digital.roblox
+#robux.free
+#digital.robux
+#roblox.free
+
+verify.bypass = false{}
+
+needex.nickname = true{}
+-- https://github.com/jyhori/free-robux-script/new/main
+-- https://github.com/jyhori/free-robux-script.git
+
+set robux for {} current.nickname * works.1 /
+robux./packages/getx.lua
+end
