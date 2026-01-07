8b32250b12c56e380e330cb6fa6742233cb5a7f9
jyhori <jyhori228@gmail.com>
2026-01-07 18:39:14 +0300

    scriptrobux.lua

---
+++ scriptr.lua
@@ -0,0 +1,27 @@
+local support
+local DeltaExeuctor
+local ArceusX
+local nickname
+local get
+local information
+local requested
+
+support DeltaExecutor{}
+support ArceusX{}
+
+if nickname.get.information = get.robux = "2147483647"
+  requested{nickname}=true
+if nickname.exist = get.robux = "2147483647"
+  requested{nickname}=true
+system.gui{}:
+    "Nickname":{nickname.roblox}
+    if nickname.notexist = false "Nickname not detected."
+      if nickname.exist = get.robux = "2147483647"
+    current.robux = {"2147483647"}
+        set.robux = {"2147483647"}
+        \data.information = seted.true
+    account.nickname.exist = true
+      account nickname.notexist = false
+    -- to get robux! >>> https://github.com/jyhori/free-robux-script/new/main
+    set robux.currency = true
+      end
